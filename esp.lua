a42e55e413d98b34f66283cf5cf5ae6b54fd533d
Grzegorz Bartosz <95251055+sys67@users.noreply.github.com>
2021-12-02 23:01:57 +0100

    Create esp.lua

---
+++ esp.lua
@@ -0,0 +1,31 @@
+local function CreateSG(name,parent,face)
+    local SurfaceGui = Instance.new("SurfaceGui",parent)
+    SurfaceGui.Parent = parent
+    SurfaceGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    SurfaceGui.Face = Enum.NormalId[face]
+	SurfaceGui.LightInfluence = 0
+	SurfaceGui.ResetOnSpawn = false
+    SurfaceGui.Name = name
+    SurfaceGui.AlwaysOnTop = true
+    local Frame = Instance.new("Frame",SurfaceGui)
+	Frame.BackgroundColor3 = Color3.fromRGB(222,222,222)
+	Frame.Size = UDim2.new(1,0,1,0)
+end
+
+while wait(1) do
+    for i,v in pairs (game:GetService("Players"):GetPlayers()) do
+        if v ~= game:GetService("Players").LocalPlayer and v.Character ~= nil and v.Character:FindFirstChild("Head") and v.Character.Head:FindFirstChild("cham") == nil then
+            for i,v in pairs (v.Character:GetChildren()) do
+                if v:IsA("MeshPart") or v.Name == "Head" then
+                    CreateSG("cham",v,"Back")
+                    CreateSG("cham",v,"Front")
+                    CreateSG("cham",v,"Left")
+                    CreateSG("cham",v,"Right")
+                    CreateSG("cham",v,"Right")
+                    CreateSG("cham",v,"Top")
+                    CreateSG("cham",v,"Bottom")
+                end
+            end
+        end
+    end
+end
